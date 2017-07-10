17da4efb313d61d6a7f440ea18a1d08b037d5579
福崎 結生 <youfose@gmail.com>
2017-07-11 05:26:44 +0900

    未入力の警告メッセージ機能追加中。未動作。

---
+++ public/main.js
@@ -0,0 +1,13 @@
+"use strict";
+window.onload = function () {
+  var = form = document.forms;
+  switch (form) {
+    case form.name =="": alert("件名が未入力です!");
+      break;
+    case form.title =="": alert("用件が未入力です!");
+      break;
+    case form.body =="": alert("あなた何しているのですか???");
+      break;
+    case form =="": alert("全部入力されていません!");
+  };
+};
