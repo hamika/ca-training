2b5ad0671c08d2cac6ff7e6a7c4e7a8d6c0684ba
福崎 結生 <youfose@gmail.com>
2017-07-12 05:41:05 +0900

    見た目の補正

--- public/main.js
+++ public/main.js
@@ -1,26 +1,26 @@
-// 'use strict';
+'use strict';
 
 function check(){
   var flag = 0;
   if (document.form.name.value == ""){
     flag = 1;
-  }
-	else if (document.form.title.value == ""){
-    flag = 1;
-  }
-	else if (document.form.body.value == ""){
-    flag = 1;
+  }else if
+    (document.form.title.value == ""){
+      flag = 1;
+  }else if
+    (document.form.body.value == ""){
+      flag = 1;
   };
 
-	if (flag){
+  if (flag){
     window.alert('必須項目に未入力がありました');
-		return false;
-	}
-	else{
-		return true;
-	};
+      return false;
+  }else{
+    return true;
+  };
 }
 
+// 本当はこっちの文を使いたい
 // window.onload = function () {
 //   var = form = document.forms;
 //   switch (form) {
